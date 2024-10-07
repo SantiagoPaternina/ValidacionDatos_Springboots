5aadce59133820d9a62dbd7454db5a53fffbfd16
SantiagoPaternina <santipater692@gmail.com>
2024-10-07 12:05:15 -0500

    Add files via upload

---
+++ api_surag5_20242-main/src/main/java/com/example/ServidorSura5/HELPERS/Licuadora.java
@@ -0,0 +1,20 @@
+package com.example.ServidorSura5.HELPERS;
+
+public class Licuadora
+{
+    //diseñar un proceso para sumar dos numeros
+    public void licuadora(){
+
+        int numeroUno=10;
+        int numeroDos=8;
+        int resultado=numeroUno+numeroDos;
+        System.out.println("La suma es: "+resultado);
+
+
+    }
+
+    public Integer sumar(int numero1, int numero2){
+        int suma=numero1+numero2;
+        return suma;
+    }
+}
